4f1f69dfd35abe7cc17e4754485d43f24bcd4885
linus-berg <linus@fenix.me.uk>
2024-04-13 18:06:23 +0200

    Fixed SL script.

--- scripts/get-sl.sh
+++ scripts/get-sl.sh
@@ -2,6 +2,7 @@
 #
 #
 #
+STATION=$1
 curl -X 'GET' \
-  'https://transport.integration.sl.se/v1/sites/$1/departures?transport=BUS&forecast=1200' \
+  "https://transport.integration.sl.se/v1/sites/${STATION}/departures?transport=BUS&forecast=1200" \
   -H 'Content-Type: application/json' -o sl.json
